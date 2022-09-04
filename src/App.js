600aebd70c217ebd09daa0a8cd4e8a3a8d4b0dc5
FacuNGodoy <facundo5_94@hotmail.com>
2022-09-04 18:24:46 -0300

    Desafio4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.png';
 import './App.css';
 import NavBarBoots from './components/NavBarBoots/NavBarBoots';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import TarjetaItem from './components/TarjetaItem/TarjetaItem';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBarBoots/>
       <ItemListContainer />
       <header className="App-header">
+        <TarjetaItem/>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,22 @@
+import {useState} from "react"
+import './Counter.css'
+
+const Counter = () => {
+    const stock = 10
+    const [carrito, setCarrito] = useState(0)
+
+    const sumar = () => carrito <= stock -1 ? setCarrito(carrito + 1) : alert(`Se alcanzó el máximo de stock.`)
+    const restar = () => carrito > 0 ? setCarrito(carrito - 1) : alert(`El número debe ser positivo.`)
+    
+  return (
+    <>
+    <div className="contador">
+        <button onClick={restar} className="carrito-Bot">-</button>
+        <div className="carrito-Num">{carrito}</div>
+        <button onClick={sumar} className="carrito-Bot">+</button>
+    </div>
+    </>
+  )
+}
+
+export default Counter
